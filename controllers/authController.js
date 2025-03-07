06fc65a7e3ef857898dd74237c73cd543a89cc51
Jinane <jinanealayli@gmail.com>
2025-03-07 14:58:22 +0200

    Fix cookie settings for Vercel

--- controllers/authController.js
+++ controllers/authController.js
@@ -35,10 +35,11 @@ export const login = async (req, res) => {
  
     res.cookie("token", token, {
         httpOnly: true, 
-        secure: false,  
-        sameSite: "Strict",  
+        secure: process.env.NODE_ENV === "production",   
+        sameSite: "None",   
         maxAge: 3600000, 
     });
+    
 
     res.json({ message: "Login successful" });
 };
